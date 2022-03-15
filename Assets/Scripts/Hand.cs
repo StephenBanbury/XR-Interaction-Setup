f99444f068369de2405516220a984c2101963974
Stephen Banbury <sbanbury@gmail.com>
2022-03-15 00:11:26 +0000

    Script hand movement and rotation with variable offsets

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    [SerializeField] private float speed;
-
+    // Animation
+    [SerializeField] private float animationSpeed;
     Animator _animator;
     SkinnedMeshRenderer _mesh;
     private float _gripTarget;
@@ -19,15 +19,52 @@ public class Hand : MonoBehaviour
     private static readonly int Grip = Animator.StringToHash(AnimatorGripParam);
     private static readonly int Trigger = Animator.StringToHash(AnimatorTriggerParam);
 
+    // Physics movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform _followTarget;
+    private Rigidbody _body;
+
     void Start()
     {
+        // Animation
         _animator = GetComponent<Animator>();
         _mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        // Physics Movement
+        _followTarget = followObject.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+
+        // Teleport Hands
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
     }
     
     void Update()
     {
         AnimateHand();
+
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // Position
+        var positionWithOffset = _followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        //Rotation
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(_body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotationSpeed);
     }
 
     internal void SetGrip(float v)
@@ -45,13 +82,13 @@ public class Hand : MonoBehaviour
         if (_gripCurrent != _gripTarget)
         {
             Debug.Log($"AnimateHand ({_animator.name}) - gripCurrent:{_gripCurrent}, gripTarget:{_gripTarget}");
-            _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget, Time.deltaTime * speed);
+            _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget, Time.deltaTime * animationSpeed);
             _animator.SetFloat(AnimatorGripParam, _gripCurrent);
         }
         if (_triggerCurrent != _triggerTarget)
         {
             Debug.Log($"AnimateHand ({_animator.name}) - gripCurrent:{_triggerCurrent}, gripTarget:{_triggerTarget}");
-            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget, Time.deltaTime * speed);
+            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget, Time.deltaTime * animationSpeed);
             _animator.SetFloat(AnimatorTriggerParam, _triggerCurrent);
         }
     }
